63ad955bafa20529721b40ab899a1ef8bea410a5
brunhilde <brunhilde.vink@oneshoe.nl>
2021-04-09 17:25:52 +0200

    Add querys

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,7 +1,59 @@
+import { server } from "../../lib/api";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData,
+} from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBaths
+      numOfBeds
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 export interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-  return <h2>{title}</h2>;
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data);
+  };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<DeleteListingData>({
+      query: DELETE_LISTING,
+      variables: {
+        id: "60706c803c3e3b46ef15c9f5",
+      },
+    });
+    console.log(data);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings</button>
+      <button onClick={deleteListing}>Delete Listing</button>
+    </>
+  );
 };
