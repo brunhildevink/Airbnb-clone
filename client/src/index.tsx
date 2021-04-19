63802227c92bd1ce72cf78bff22bcc1747193eac
brunhilde <brunhildevink1995@gmail.com>
2021-04-19 09:58:35 +0200

    Add apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import { Listings } from "./sections";
 
+const client = new ApolloClient({ uri: "/api", cache: new InMemoryCache() });
+
 render(
   <React.StrictMode>
-    <Listings title="TinyHouse Listings" />
+    <ApolloProvider client={client}>
+      <Listings title="TinyHouse Listings" />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
