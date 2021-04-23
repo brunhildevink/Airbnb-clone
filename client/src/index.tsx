b3bca3676d36725b92dfc9970644ec5d771173b2
brunhilde <brunhildevink1995@gmail.com>
2021-04-23 14:55:37 +0200

    Revert "Add apollo client"
    This reverts commit 63802227c92bd1ce72cf78bff22bcc1747193eac.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import { Listings } from "./sections";
 
-const client = new ApolloClient({ uri: "/api", cache: new InMemoryCache() });
-
 render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <Listings title="TinyHouse Listings" />
-    </ApolloProvider>
+    <Listings title="TinyHouse Listings" />
   </React.StrictMode>,
   document.getElementById("root")
 );
