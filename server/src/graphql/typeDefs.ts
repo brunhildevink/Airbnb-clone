f51a7138bffbd92775b8053d741bc3c6654968d6
brunhilde <brunhildevink1995@gmail.com>
2021-06-25 15:45:59 +0200

    Implement things

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,27 +1,12 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-  type Listing {
-    id: ID!
-    address: String!
-    admin: String!
-    bookings: [ID]!
-    city: String!
-    country: String!
-    description: String!
-    host: String!
-    image: String
-    numOfGuests: Int
-    price: Int!
-    title: String!
-    type: String
-  }
-
   type Query {
-    listings: [Listing!]!
+    authUrl: String!
   }
 
   type Mutation {
-    deleteListing(id: ID!): Listing!
+    logIn: String!
+    logOut: String!
   }
 `;
