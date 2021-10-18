957a4e28517aa98d9e3bebc5150ae22565815cc0
brunhilde <brunhildevink1995@gmail.com>
2021-10-18 13:19:11 +0200

    Adding login session cookie on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,19 @@ import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
 import { typeDefs, resolvers } from "./graphql";
 import * as dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
 
   server.applyMiddleware({ app, path: "/api" });
